e64636f7682ecc04a55e6938eb919b5df0afebae
Krzysztof Duś <krecik11120@gmail.com>
2016-08-19 01:41:11 +0200

    repozytorium jquery

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$(function(){
+	$("span:odd").css('color','red');
+/*change color*/
+	var clas = $('p');
+	clas.each(function(index, element) { 
+		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+		$(element).append(button) 
+	});
+/*add new element*/
+	$("button").click(function(){ alert($(this).attr("data-tmp")); 
+	});
+/*add event handler*/
+});
+
+
+
+
+
+ 
+	
